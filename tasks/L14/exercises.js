93a263eda3acb6b0a17c8e3a9e78da527b78cc80
Marina Shehova <msmarina90@gmail.com>
2015-12-01 17:06:54 +0200

    changed on 1 of Dec at work

--- tasks/L14/exercises.js
+++ tasks/L14/exercises.js
@@ -1,49 +1,40 @@
 "use strict"
-//task 1 works
-function greeting(name) {
 
+var personGreeting; 
+
+  function greeting(name) {
     return "Hello, " + name + "!";
 }
-
 greeting("Jeni");
-//task 2 works
-function Person(fname, lname) {
+
+  function Person(fname, lname) {
     this.firstName = fname;
     this.lastName = lname;
-    // 6 works
     this.isPolite = true;
-    // 7 ????
-    this.bePolite = function () {
-        if (this.isPolite === true) {
-            return true;
-        } else if (this.isPolite === false)
-            return false;
-    }
-    this.fullName = fname + lname;
-    // 5 works
-    this.introduce = function () {
-       // 8 samo pyrvoto se poluchava s false ni6to ne stava
-        if (this.isPolite === true) {
-            return "Казвам се Бойко Борисов!";
-        } else if (this.isPolite == false) {
+    this.fullName = fname + " " + lname;
+    this.introduce = function() {
+       return "Здравейте, kазвам се " + this.fullName;
+    } 
+   this.bePolite = function (isPolite) {
+     if (isPolite == false) {
+	   return "Аз съм " + this.firstName;
+  }
+  }
 
-            return "Аз съм Бойко!";
-        }
-        return "Здравейте, казвам се " + this.firstName + " " + this.lastName;
-    }
-    this.greet = function(other) {
-        
-    }
+  this.greet = function(otherPerson) {
+	  var boiko = new Person("Бойко", "Борисов");
+      var lili = new Person("Лили", "Иванова");
+	   boiko.introduce();
+	  lili.bePolite(false); 
+	  lili.greet(boiko);
+	  lili.introduce();
+	// return "Zdravei, " + otherPerson + "kazvam se " + this.firstName;  
+  }
 
+   this.personGreeting = function () {
+	return "Hello, " + this.fullName;
+}
+    personGreeting(new Person("Бойко", "Борисов"));
 }
 
 
-// it is not working 4, 3
-
-var personGreeting = function Person() {
-    var beFormal = true;
-
-    return "Hello, " + greeting;
-}
-
-personGreeting(new Person("Бойко", "Борисов"));
